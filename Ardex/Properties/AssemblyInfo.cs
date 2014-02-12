8c05fc8f019a0d600084542bdc28883f2a8e710f
Kirill <kirill@ardex.com.au>
2014-02-12 12:21:54 +1100

    Fix AssemblyInfo (portable support)

--- Ardex/Properties/AssemblyInfo.cs
+++ Ardex/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5a4616a1-0f9d-4a2a-9f79-741324b0d7ab")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
