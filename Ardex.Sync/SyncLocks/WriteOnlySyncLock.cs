d7b642a89e976f42b5473dc3a654329d612e4bf7
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2014-03-28 19:38:01 +1100

    InglisSalesApp-driven changes

---
+++ Ardex.Sync/SyncLocks/WriteOnlySyncLock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Ardex.Sync.SyncLocks
+{
+    public class WriteOnlySyncLock : ISyncLock
+    {
+        private readonly object Lock = new object();
+
+        public IDisposable ReadLock()
+        {
+            return Disposables.Null;
+        }
+
+        public IDisposable WriteLock()
+        {
+            Monitor.Enter(this.Lock);
+
+            return Disposables.Once(() => Monitor.Exit(this.Lock));
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
